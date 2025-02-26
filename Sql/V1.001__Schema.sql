53ece5b324dcc9edae4d5c362a457bb95a6e94bf
andreas14101 <andreasshansen@live.dk>
2025-02-26 11:08:54 +0100

    added middle name to student and generated a migration and sql file that matches

---
+++ Sql/V1.001__Schema.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Students] ADD [MiddleName] nvarchar(max) NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250226095522_student-middlename', N'9.0.2');
+
+COMMIT;
+GO
+
