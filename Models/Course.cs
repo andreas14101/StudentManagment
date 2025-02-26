04c08ce1fcfd99a4d6effa8a896d0d85502a5908
andreas14101 <andreasshansen@live.dk>
2025-02-26 11:34:52 +0100

    added instructor and made a migration and sql file together with an update to README.md

--- Models/Course.cs
+++ Models/Course.cs
@@ -7,5 +7,6 @@ public class Course
     [Key]
     public Guid Id { get; set; }
     public string Title { get; set; }
+    public Instructor? Instructor { get; set; }
     public float Credits { get; set; }
 }
