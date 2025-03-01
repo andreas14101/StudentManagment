44a9e9e7472e7af1a5e82222874a9654ffafe4a8
andreas14101 <andreasshansen@live.dk>
2025-03-01 15:49:13 +0100

    created the initial models, DB context, schema, migration and sql file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StudentManagment;
+
+using (StudentManagmentContext context = new StudentManagmentContext())
+{
+    Console.WriteLine("Hello, World!");
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # StudentManagment
+
+this branch contains the initial database schema for the student management system, all ids in the schema use Guid seeing as it does not reveal much about the system like if ids were to be integers
