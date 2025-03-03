dd37e8709bfdfef5816d742196f864a712c2f163
andreas14101 <andreasshansen@live.dk>
2025-03-03 10:58:17 +0100

    added key data annotation to Id in Departoment.cs

--- Models/Departoment.cs
+++ Models/Departoment.cs
@@ -1,7 +1,10 @@
-﻿namespace StudentManagment.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagment.Models;
 
 public class Departoment
 {
+    [Key]
     public Guid Id { get; set; }
     public string Name { get; set; }
     public double Dudget { get; set; }
