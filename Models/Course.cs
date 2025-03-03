bc0e82a2d7f955aec6b4c933b6705466194e7c8f
andreas14101 <andreasshansen@live.dk>
2025-03-03 09:51:44 +0100

    made the initial models, migration and sql files

---
+++ Models/Course.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagment.Models;
+
+public class Course
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public int Credits { get; set; }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # StudentManagment
+
+this branch contains the initial schema for state-based
